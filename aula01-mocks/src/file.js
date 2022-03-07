3779d3d2641c116f956f88617d62ad51187d65ef
Raphael Berger <raphaelmb@gmail.com>
2022-03-07 16:44:50 -0300

    Adding validation

--- aula01-mocks/src/file.js
+++ aula01-mocks/src/file.js
@@ -1,9 +1,17 @@
 const { readFile } = require("fs/promises");
 const { join } = require("path");
+const { error } = require("./constants");
+
+const DEFAULT_OPTION = {
+  maxLines: 3,
+  fields: ["id", "name", "profession", "age"],
+};
 
 class File {
   static async csvToJson(filePath) {
     const content = await File.getFileContent(filePath);
+    const validation = File.isValid(content);
+    if (!validation.valid) throw new Error(validation.error);
     return content;
   }
 
@@ -11,9 +19,34 @@ class File {
     const filename = join(__dirname, filePath);
     return (await readFile(filename)).toString("utf-8");
   }
+
+  static isValid(csvString, options = DEFAULT_OPTION) {
+    const [header, ...fileWithoutHeader] = csvString.split("\n");
+    const isHeaderValid = header === options.fields.join(",");
+    if (!isHeaderValid) {
+      return {
+        error: error.FILE_FIELDS_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    const isContentLengthAccepted =
+      fileWithoutHeader.length > 0 &&
+      fileWithoutHeader.length <= options.maxLines;
+    if (!isContentLengthAccepted) {
+      return {
+        error: error.FILE_LENGTH_ERROR_MESSAGE,
+        valid: false,
+      };
+    }
+
+    return { valid: true };
+  }
 }
 
 (async () => {
   const result = await File.csvToJson("./../mocks/threeItems-valid.csv");
+  // const result = await File.csvToJson("./../mocks/invalid-header.csv");
+  // const result = await File.csvToJson("./../mocks/fourItems-invalid.csv");
   console.log("result", result);
 })();
