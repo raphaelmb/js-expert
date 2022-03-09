7c1983b11cbddef19c16e640e59b083cc93c32c2
Raphael Berger <raphaelmb@gmail.com>
2022-03-08 22:00:30 -0300

    Updates and test

---
+++ aula01-mocks/index.test.js
@@ -0,0 +1,44 @@
+const { error } = require("./src/constants");
+const File = require("./src/file");
+const { rejects, deepStrictEqual } = require("assert");
+
+(async () => {
+  {
+    const filePath = "./mocks/emptyFile-invalid.csv";
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+    await rejects(result, rejection);
+  }
+  {
+    const filePath = "./mocks/fourItems-invalid.csv";
+    const rejection = new Error(error.FILE_LENGTH_ERROR_MESSAGE);
+    const result = File.csvToJson(filePath);
+    await rejects(result, rejection);
+  }
+  {
+    Date.prototype.getFullYear = () => 2020;
+    const filePath = "./mocks/threeItems-valid.csv";
+    const result = await File.csvToJson(filePath);
+    const expected = [
+      {
+        name: "John Doe",
+        id: 123,
+        profession: "Javascript Developer",
+        birthDay: 1995,
+      },
+      {
+        name: "Jane Doe",
+        id: 321,
+        profession: "Javascript Specialist",
+        birthDay: 1940,
+      },
+      {
+        name: "Joaozinho",
+        id: 231,
+        profession: "Java Developer",
+        birthDay: 1990,
+      },
+    ];
+    deepStrictEqual(JSON.stringify(result), JSON.stringify(expected));
+  }
+})();
