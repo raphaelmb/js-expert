2b0b1096cf032b41f3e25aa16d58a776ecd24aac
Raphael Berger <raphaelmb@gmail.com>
2022-04-27 14:49:33 -0300

    Final updates + testing

--- modulo03/demo02-esmodules-internationalization/src/index.js
+++ modulo03/demo02-esmodules-internationalization/src/index.js
@@ -2,6 +2,7 @@ import database from "../database.json" assert { type: "json" };
 
 import Person from "./person.js";
 import TerminalController from "./terminalController.js";
+import { save } from "./repository.js";
 
 const DEFAULT_LANG = "pt-BR";
 const STOP_TERM = ":q";
@@ -18,7 +19,8 @@ async function mainLoop() {
       return;
     }
     const person = Person.generateInstanceFromString(answer);
-    console.log("person", person.formatted(DEFAULT_LANG));
+    terminalController.updateTable(person.formatted(DEFAULT_LANG));
+    await save(person);
     return mainLoop();
   } catch (error) {
     console.error("Deu ruim**", error);
